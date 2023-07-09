5a0c206ad71853bf8bccbdea612065b6035682ad
bertrand751979 <bertrandassinou@gmail.com>
2023-07-09 09:16:55 +0200

    version firestore

---
+++ app/src/main/java/com/example/bookapplicationmvvmretrofitroom/OnButtonDeleteActionClicked.java
@@ -0,0 +1,7 @@
+package com.example.bookapplicationmvvmretrofitroom;
+
+import com.example.bookapplicationmvvmretrofitroom.model.model.Item;
+
+public interface OnButtonDeleteActionClicked {
+    void deleteBook (Item item);
+}
